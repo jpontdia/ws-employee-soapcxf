6d2f86600c3fd6fe519e8856838111e6c61112c0
Joaquin Ponte <joaquin.ponte@gmail.com>
2020-09-26 03:58:08 -0400

    Added RestAssured for test cases

--- src/test/java/com/jpworks/datajdbc/employee/SoapTests.java
+++ src/test/java/com/jpworks/datajdbc/employee/SoapTests.java
@@ -1,22 +1,84 @@
 package com.jpworks.datajdbc.employee;
 
-import com.jpworks.datajdbc.service.EmployeeEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.FileCopyUtils;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+
+import static io.restassured.RestAssured.given;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.Matchers.containsString;
 
 @Slf4j
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SoapTests {
+    @LocalServerPort
+    private int port;
 
     @Autowired
-    private EmployeeEndpoint employeeEndpoint;
+    private ResourceLoader resourceLoader;
 
     @Test
-    public void contextLoads(){
-        assertThat(employeeEndpoint).isNotNull();
+    public void wsdl(){
+        given()
+            .when()
+                .get("http://localhost:" + port +"/soap/service/employee?wsdl")
+                .then()
+                .assertThat()
+                .body(containsString("wsdl:definitions xmlns:xsd="));
     }
+
+    @Test
+    public void listOfServices(){
+        given()
+                .when()
+                .get("http://localhost:" + port +"/soap")
+                .then()
+                .assertThat()
+                .body(containsString("Available SOAP services:"));
+    }
+
+    @Test
+    public void getEmployeeById(){
+        Resource res = resourceLoader.getResource("classpath:getEmployeeById.xml");
+        given()
+            .contentType("text/xml;charset=UTF-8")
+            .body(asString(res))
+        .when()
+            .post("http://localhost:" + port +"/soap/service/employee")
+        .then()
+            .assertThat()
+            .body(containsString("EmployeeResponse"));
+    }
+
+    @Test
+    public void getEmployeeByName(){
+        Resource res = resourceLoader.getResource("classpath:getEmployeeByName.xml");
+        given()
+                .contentType("text/xml;charset=UTF-8")
+                .body(asString(res))
+                .when()
+                .post("http://localhost:" + port +"/soap/service/employee")
+                .then()
+                .assertThat()
+                .body(containsString("EmployeesResponse"));
+    }
+
+    public static String asString(Resource resource) {
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
 }
